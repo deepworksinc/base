4ee303a5c122b619d1171b0ce7de67b795988d31
Shane Earley <shanejearley@gmail.com>
2022-10-28 15:01:21 -0400

    Add back bluetooth policy

--- apps/ios/Base/Base-Bridging-Header.h
+++ apps/ios/Base/Base-Bridging-Header.h
@@ -5,6 +5,6 @@
 #ifndef Base_Bridging_Header_h
 #define Base_Bridging_Header_h
 
-#import “DetrendedFluctuationWrapper.h”
+#import "DetrendedFluctuationWrapper.h"
 
 #endif /* Base_Bridging_Header_h */
